b58df4b856eaa3f0669f39ac884355a9acdcfbb8
LuannMuryell <luannmuryellr@gmail.com>
2026-04-24 13:14:28 -0300

    Novo model e tabela de servidores

---
+++ app/Servidor.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Servidor extends Model
+{
+    use HasFactory;
+
+    protected $table = 'servidores';
+
+    protected $fillable = [
+        'nome',
+        'cpf',
+        'matricula'
+    ];
+}
