6546f9a626404932252506ea5310edefb83884b5
LuannMuryell <luannmuryellr@gmail.com>
2026-04-27 14:47:17 -0300

    Altera a data de criação da migration para cria-la após a tabela de servidores

--- database/migrations/2026_04_24_114928_create_votacoes_table.php
+++ database/migrations/2026_04_24_114928_create_votacoes_table.php
@@ -6,36 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('votacoes', function (Blueprint $table) {
             $table->engine = 'InnoDB';
             $table->bigIncrements('id');
-      //    $table->unsignedBigInteger('votacao_realizada');
             $table->foreign('eleicoes_id')->references('id')->on('eleicoes');
-            $table->string('eleitor_matricula')->unique();
             $table->unsignedBigInteger('eleicoes_id');
             $table->char('tipo_voto');
             $table->string('voto_candidato_id')->nullable();
-            $table->string('eleitor_nome');
-            $table->string('eleitor_cpf');
-            $table->string('eleitor_lotacao')->nullable();
-            $table->string('eleitor_cargo_funcao')->nullable();
-            $table->string('eleitor_IP_acesso');
+            $table->foreignId('servidor_id')->constrained('servidores')->cascadeOnDelete();
+            $table->string('servidor_ip_acesso');
             $table->timestamps();
+
+            $table->unique(['eleicoes_id', 'servidor_id']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('votacoes');
