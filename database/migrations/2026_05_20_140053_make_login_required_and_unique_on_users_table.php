3aedb31208283c9a226def9088a8019cec0bb6ac
Giuli.ilustre <giuli.ilustre@pmsps.local>
2026-05-20 14:15:38 -0300

    Adiciona obrigatoriedade no campo login

---
+++ database/migrations/2026_05_20_140053_make_login_required_and_unique_on_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+             $table->string('login')
+                  ->nullable(false)
+                  ->unique()
+                  ->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('login')
+                  ->nullable()
+                  ->change();
+
+            $table->dropUnique(['login']);
+        });
+    }
+};
