5667a6245e37020c57f5538961c0d3675f364fb3
Giuli.ilustre <giuli.ilustre@pmsps.local>
2026-05-25 17:25:33 -0300

    cria comando para criar backup de banco

---
+++ app/Console/Commands/BackupDatabase.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class BackupDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'backup:database';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Realiza backup do banco de dados';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        
+        $database = env('DB_DATABASE');
+        $username = env('DB_USERNAME');
+        $password = env('DB_PASSWORD');
+        $host = env('DB_HOST');
+
+        $filename = 'backup_' . date('Y-m-d_H-i-s') . '.sql';
+
+        $path = storage_path('app/backups/' . $filename);
+
+        if (!file_exists(storage_path('app/backups'))) {
+            mkdir(storage_path('app/backups'), 0755, true);
+        }
+
+        $mysqldump = env('MYSQL_DUMP_PATH');
+
+         $command = sprintf(
+            '"%s" --user=%s --password=%s --host=%s %s > "%s"',
+            $mysqldump,
+            $username,
+            $password,
+            $host,
+            $database,
+            $path
+        );
+
+        exec($command, $output, $resultCode);
+
+        if ($resultCode !== 0) {
+            $this->error('Erro ao gerar backup.');
+            return;
+        }
+
+        $this->info('Backup realizado com sucesso!');
+    }
+}
